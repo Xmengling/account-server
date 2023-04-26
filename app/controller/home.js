b046f355b702f3789ef3d9b3b4095b97065db8bb
c28381 <cheng.mengling@h3c.com>
2023-04-26 11:44:48 +0800

    feat: 连接数据库，实现增删改查

--- app/controller/home.js
+++ app/controller/home.js
@@ -13,13 +13,8 @@ class HomeController extends Controller {
   }
   async user() {
     const { ctx } = this
-    const { id } = ctx.params
-    const { name, slogan } = await ctx.service.home.user()
-
-    ctx.body = {
-      name,
-      slogan
-    }
+    const result = await ctx.service.home.user()
+    ctx.body = result
   }
   async add() {
     const { ctx } = this;
@@ -29,6 +24,61 @@ class HomeController extends Controller {
       title
     };
   }
+  async addUser() {
+    const { ctx } = this;
+    const { name } = ctx.request.body;
+    try {
+      const result = await ctx.service.home.addUser(name)
+      ctx.body = {
+        code: 200,
+        msg: '添加成功',
+        data: null
+      }
+    } catch (error) {
+      ctx.body = {
+        code: 500,
+        msg: '添加失败',
+        data: null
+      }
+    }
+  }
+  async editUser() {
+    const { ctx } = this;
+    const { id, name } = ctx.request.body;
+    try {
+      const result = ctx.service.home.editUser(id, name)
+      ctx.body = {
+        code: 200,
+        msg: '添加成功',
+        data: null
+      }
+    } catch (error) {
+      ctx.body = {
+        code: 500,
+        msg: '添加失败',
+        data: null
+      }
+    }
+  }
+  async deleteUser() {
+    const { ctx } = this
+    const { id } = ctx.request.body
+    try {
+      const result = ctx.service.home.deleteUser(id)
+      ctx.body = {
+        code: 200,
+        msg: '删除成功',
+        data: null
+      }
+    } catch (error) {
+      ctx.body = {
+        code: 500,
+        msg: '删除失败',
+        data: null
+      }
+    }
+  }
+
 }
 
 module.exports = HomeController;
--- app/service/home.js
+++ app/service/home.js
@@ -4,9 +4,49 @@ const Service = require('egg').Service
 
 class HomeService extends Service {
   async user() {
-    return {
-      name: '111',
-      slogan: '222'
+    const { ctx, app } = this;
+    const QUERY_STR = 'id, name';
+    let sql = `select ${QUERY_STR} from list`;
+    try {
+      const result = await app.mysql.query(sql);
+      return result;
+    } catch {
+      console.log('error', error);
+      return null; 
+    }
+  }
+  async addUser(name) {
+    const { ctx, app } = this;
+    try {
+      const result = await app.mysql.insert('list', { name })
+      return result
+    } catch (error) {
+      console.log('error', error)
+      return null
+    }
+  }
+  async editUser(id, name) {
+    const { ctx, app } = this
+    try {
+      let result = await app.mysql.update('list', { name }, {
+        where: {
+          id
+        }
+      });
+      return result
+    } catch (error) {
+      console.log('error', error)
+      return null
+    }
+  }
+  async deleteUser(id) {
+    const { ctx, app } = this;
+    try {
+      let result = await app.mysql.delete('list', { id });
+      return result
+    } catch (error) {
+      console.log('error', error)
+      return null
     }
   }
 }
